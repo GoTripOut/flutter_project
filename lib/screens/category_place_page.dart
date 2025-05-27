ab17cbcaa5034462723819ea7543289cbba5459e
mindoll1101 <mindoll1101@kangwon.ac.kr>
2025-05-28 01:02:27 +0900

    장소 리스트 추천 시 위젯 사이즈 오버플로우 문제 해결

--- lib/screens/category_place_page.dart
+++ lib/screens/category_place_page.dart
@@ -59,96 +59,124 @@ class _CategoryPlaceListPageState extends State<CategoryPlaceListPage> {
   @override
   Widget build(BuildContext context) {
     final categoryIcon = _getCategoryIcon(widget.categoryName);
-
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-          title: Row(
-            children: [
-              Text('${widget.categoryName} 주변 장소',
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
+        title: Row(
+          children: [
+            Text('${widget.categoryName} 주변 장소',
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
               ),
-              const SizedBox(width: 8.0), // 텍스트와 아이콘 사이 간격
-              if (categoryIcon != null) categoryIcon,
-            ],
-          )),
+            ),
+            const SizedBox(width: 8.0), // 텍스트와 아이콘 사이 간격
+            if (categoryIcon != null) categoryIcon,
+          ],
+        )
+      ),
       body: placeList.isNotEmpty
-          ? ListView.builder(
-          itemCount: placeList.length,
-          itemBuilder: (context, index) {
-            final place = placeList[index];
-            final String placeName = place['store_name'];
-            final double latitude = place['y'];
-            final double longitude = place['x'];
-            final status = place['status'];
-            final statusDescription = place['status_description'];
-            final visitorReviewScore = place['visitorReviewScore'];
-            final visitorReviewCount = place['visitorReviewCount'];
-            final aiScore = place['AI_score'];
+        ? ListView.builder(
+        itemCount: placeList.length,
+        itemBuilder: (context, index) {
+          final place = placeList[index];
+          final String placeName = place['store_name'];
+          final double latitude = place['y'];
+          final double longitude = place['x'];
+          final status = place['status'];
+          final statusDescription = place['status_description'];
+          final visitorReviewScore = place['visitorReviewScore'];
+          final visitorReviewCount = place['visitorReviewCount'];
+          final aiScore = place['AI_score'];
 
-            double rating = 0;
-            if (visitorReviewScore != null) {
-              rating = double.tryParse(visitorReviewScore) ?? 0.0;
-            }
+          double rating = 0;
+          if (visitorReviewScore != null) {
+            rating = double.tryParse(visitorReviewScore) ?? 0.0;
+          }
 
-            return ListTile(
-                title: Row(
-                  children: [
-                    Text(placeName),
-                    const SizedBox(width: 3.0,),
-                    if (visitorReviewScore != null)
-                      RatingBarIndicator(
-                        rating: rating,
-                        itemBuilder: (context, _) => const Icon(
-                          Icons.star,
-                          color: Colors.amber,
+          return ListTile(
+            title: Stack(
+              children: [
+                SizedBox(
+                  width: screenWidth * 0.8,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(placeName),
+                          ],
                         ),
-                        itemCount: 5,
-                        itemSize: 10.0,
-                        direction: Axis.horizontal, // 별들 가로로 배치
-                      ),
-                    const SizedBox(width: 3.0), // 별점과 점수 사이의 공간 추가
-                      if (visitorReviewScore != null)
-                      Text(
-                        '$visitorReviewScore (${visitorReviewCount != null ? "$visitorReviewCount" : 0})',
-                        style: const TextStyle(fontSize: 12.0),
-                      ),
-                    const Spacer(), // 아이콘을 오른쪽 끝으로
-                    IconButton(
-                      iconSize: 15.0,
-                      icon: const Icon(Icons.arrow_forward_ios),
-                      onPressed: () {
-                        Navigator.pop(context,
-                            {'latitude': latitude, 'longitude': longitude});
-                      },
-                    ),
-                  ],
+                        Row(
+                          children: [
+                            if (status != null)
+                              Text(status, style: TextStyle(
+                                fontSize: 11,
+                                color: status == '오늘 휴무' || status == '영업 종료'
+                                    ? Colors.red : Colors.black,),
+                              ),
+                            if (status == null)
+                              Text(
+                                  "영업 정보가 없습니다",
+                                  style: TextStyle(
+                                      fontSize: 11
+                                  )
+                              ),
+                            const SizedBox(width: 8.0),
+                            if (statusDescription != null)
+                              Text(
+                                statusDescription,
+                                style: const TextStyle(fontSize: 12.0),
+                              ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "AI Score : $aiScore점   ",
+                              style: TextStyle(
+                                fontSize: 12,
+                              ),
+                            ),
+                            if (visitorReviewScore != null)
+                              RatingBarIndicator(
+                                rating: rating,
+                                itemBuilder: (context, _) => const Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                itemCount: 5,
+                                itemSize: 10.0,
+                                direction: Axis.horizontal, // 별들 가로로 배치
+                              ),
+                            const SizedBox(width: 3.0), // 별점과 점수 사이의 공간 추가
+                            if (visitorReviewScore != null)
+                              Text(
+                                '$visitorReviewScore (${visitorReviewCount != null ? "$visitorReviewCount" : 0})',
+                                style: const TextStyle(fontSize: 12.0),
+                              ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ),
                 ),
-                subtitle: Row(
-                  children: [
-                      Text("AI Score : $aiScore점,   ", style: TextStyle(
-                        fontSize: 11,),
-                      ),
-                    if (status != null)
-                      Text(status, style: TextStyle(
-                        fontSize: 11,
-                        color: status == '오늘 휴무' || status == '영업 종료'
-                        ? Colors.red : Colors.black,),
-                      ),
-                    if (status == null)
-                      Text("영업 정보가 없습니다", style: TextStyle(
-                          fontSize: 11)),
-                    const SizedBox(width: 8.0),
-                    if (statusDescription != null)
-                      Text(
-                        statusDescription,
-                        style: const TextStyle(fontSize: 12.0),
-                      ),
-                  ],
-                ));
-          }) : Center(
+                Positioned(
+                  right: -15,
+                  child: IconButton(
+                    iconSize: 15.0,
+                    icon: const Icon(Icons.arrow_forward_ios),
+                    onPressed: () {
+                      Navigator.pop(context, {'latitude': latitude, 'longitude': longitude});
+                    },
+                  ),
+                )
+              ],
+            ),
+          );
+        }) : Center(
           child: Text("검색 결과가 없습니다")
       ),
     );
